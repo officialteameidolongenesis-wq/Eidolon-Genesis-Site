fa9e51fb02e28fd765e1395f231ceca5345262fb
officialteameidolongenesis-wq <official.team.eidolon.genesis@gmail.com>
2025-08-10 11:48:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Eidolon-Genesis-Site/',
   plugins: [react()],
-  base: '/Eidolon-Genesis-Site/', // esattamente il nome della repo
 })
