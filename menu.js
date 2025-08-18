cca25c1f431995607a87ba39f6830a390da6f2bb
officialteameidolongenesis-wq <official.team.eidolon.genesis@gmail.com>
2025-08-18 15:00:32 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+// Gestione hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
